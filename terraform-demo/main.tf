3de675e1427b77677880a4698ceac7168fc42ace
Aayushsingh2001 <as6994770@gmail.com>
2025-04-26 07:17:00 +0530

    Added variables.tf, outputs.tf, dev.tfvars and prod.tfvars file to seperate the multible resources

--- terraform-demo/main.tf
+++ terraform-demo/main.tf
@@ -1,10 +1,51 @@
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
 resource "aws_instance" "terraform-ec2" {
-  ami = "ami-0f1dcc636b69a6438"
-  instance_type = "t2.micro"
-  count = 1
+  # ami = "ami-0f1dcc636b69a6438"
+  ami = var.ami_id
+  # instance_type = "t2.micro"
+  instance_type = var.instance_type
+  # count = 2
+  count = var.instance_count
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-aayush-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_security_group" "sg_example" {
+  name = "terraform_allow_group"
+  description = "Allowing traffic to instances"
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allowing the traffic via terraform config"
+  }
 }
---
+++ terraform-demo/variables.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+  type = string
+  default = "ami-0f1dcc636b69a6438"
+  description = "This is the AMI id of AL2 in ap-south-1 region"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "This is type of ec2 instance to launch"
+}
+
+variable "instance_count" {
+  type = number
+  default = 3
+  description = "Number of instances"
+}
+
+variable "instance_name" {
+  type = string
+  default = "Terraform-created"
+  description = "Name of instances"
+}
+
+variable "aws_region" {
+  type = string
+  default = "ap-south-1"
+  description = "aws region"
+}
+
